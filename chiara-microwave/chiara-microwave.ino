0d9917d4c74e8012b355064efd94a8a057f688d0
Anthony Valente <anthony.valente@airport-systems.com>
2021-12-02 23:07:14 +0000

    Added ultra-low current mode with interrupt

--- chiara-microwave/chiara-microwave.ino
+++ chiara-microwave/chiara-microwave.ino
@@ -1,10 +1,12 @@
 #include <TimerFreeTone.h>
+#include <avr/sleep.h>
+#include <avr/interrupt.h>
 
 const int buzzer = 0; // ATTiny pin 0
 const int led = 1; // ATTiny pin 1
 const int button = 2; // ATTiny pin 2
 
-int microwaveNoises[] = {50, 2200, 0, 2200, 0, 2200, 0, 2200, 0, 2200, 0};
+int microwaveNoises[] = {50, 2400, 0, 2400, 0, 2400, 0, 2400, 0, 2400, 0};
 int duration[] = {10000, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500};
 
 int buttonState = 1; 
@@ -19,12 +21,36 @@ void loop(){
   buttonState = digitalRead(button);
 
   if (buttonState == LOW) { 
-    digitalWrite(led, HIGH);
-    for (int thisNote = 0; thisNote < 11; thisNote++) { 
-      if (thisNote == 1) {
-        digitalWrite(led, LOW);
-      }
-      TimerFreeTone(buzzer, microwaveNoises[thisNote], duration[thisNote]); 
-    }   
+    doMicrowaveThings();
   }
-}                         
+  enterSleep();
+}
+
+void doMicrowaveThings() {
+  digitalWrite(led, HIGH);
+  for (int thisNote = 0; thisNote < 11; thisNote++) { 
+    if (thisNote == 1) {
+      digitalWrite(led, LOW);
+    }
+    TimerFreeTone(buzzer, microwaveNoises[thisNote], duration[thisNote]); 
+  }   
+}
+
+void enterSleep() {
+  GIMSK |= _BV(PCIE);                     // Enable Pin Change Interrupts
+  PCMSK |= _BV(PCINT2);                   // Use PB2 as interrupt pin
+  ADCSRA &= ~_BV(ADEN);                   // ADC off
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);    // replaces above statement
+  sleep_enable();                         // Sets the Sleep Enable bit in the MCUCR Register (SE BIT)
+  sei();                                  // Enable interrupts
+  sleep_cpu();                            // sleep
+  cli();                                  // Disable interrupts
+  PCMSK &= ~_BV(PCINT2);                  // Turn off PB2 as interrupt pin
+  sleep_disable();                        // Clear SE bit
+  ADCSRA |= _BV(ADEN);                    // ADC on
+  sei();                                  // Enable interrupts
+}
+
+ISR(PCINT0_vect) {
+
+}
